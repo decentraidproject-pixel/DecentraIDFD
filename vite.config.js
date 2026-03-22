98f78a933d6c0f92653349b43e415ee6023fcfb0
decentraidproject-pixel <decentraid.project@gmail.com>
2026-03-22 14:53:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
-*/
-
-export default {
   build: {
-    outDir: "dist"
-  }
-}
+    outDir: "dist",
+  },
+});
